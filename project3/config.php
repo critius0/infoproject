7476eda81f4f5aba58ee52f036a2c08a43bd6387
cmcgillin <cpmcgillin@gmail.com>
2016-04-21 12:13:53 -0500

    Delete config.php

--- project3/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $dbUser = "cmcgillin";
-    $dbPassword = "aj5Ykj-GpzPN";
-    $dbName = "db_cmcgillin";
-    $dbHost = "dbdev.cs.uiowa.edu";
-    $adminEmail = "colin-mcgillin@uiowa.edu";
-    $baseURL = "http://webdev.divms.uiowa.edu/~cmcgillin/infoproject/project3/";
-?>
